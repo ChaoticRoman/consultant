e509645c5cc230a92bde3b4dacd093176cf5f0af
Roman Pavelka <roman.pavelka.asi@gmail.com>
2025-02-22 17:54:43 +0000

    Basic OpenAI integration

---
+++ ai.py
@@ -0,0 +1,26 @@
+import os
+
+import openai
+
+with open(".openai_key", "r") as f:
+    os.environ["OPENAI_API_KEY"] = f.read().strip()
+
+client = openai.OpenAI()
+
+
+def chat(prompt, history=None, model="o3-mini"):
+    messages = history if history else []
+    messages.append({"role": "user", "content": prompt})
+
+    response = client.chat.completions.create(
+        model=model, messages=messages
+    )
+
+    message = response.choices[0].message
+    content = message.content.strip()
+
+    return content
+
+
+if __name__ == "__main__":
+    print(f"{chat('Tell a joke.')=}")
--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 from send import send
+from ai import chat
 
 def handle(contact, message):
-    send(contact, f"You think that {message}?!")
+    send(contact, chat(message))
