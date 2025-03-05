ddc4666f7d2f74b6c8943aba113a178fc55766b0
Roman Pavelka <roman.pavelka.asi@gmail.com>
2025-03-05 15:32:28 +0000

    Improved status page

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import hashlib
 from typing import Annotated
 
 from fastapi import FastAPI, Request, Query
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 import uvicorn
 
 from models import WebhookBody
-from handler import handle
+from handler import handle, status
 from utils import full_path
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 
 with open(full_path(".app_secret")) as f:
     app_secret = f.read().strip()
@@ -26,7 +26,7 @@ def dump(data):
         return json.dumps(data, indent=4)
 
 
-#@app.middleware("http")
+@app.middleware("http")
 async def log_requests(request: Request, call_next):
     """
     Middleware to log details of every incoming request.
@@ -62,8 +62,9 @@ def verify_webhook(data, hmac_header):
 async def auth_requests(request: Request, call_next):
     body = await request.body()
 
-    if not verify_webhook(body, dict(request.headers)['x-hub-signature-256']):
-        return JSONResponse(None, 401, {"WWW-Authenticate": "Basic"})
+    if request.url.path != "/":
+        if not verify_webhook(body, dict(request.headers).get('x-hub-signature-256')):
+            return JSONResponse(None, 401, {"WWW-Authenticate": "Basic"})
 
     response = await call_next(request)
     return response
@@ -78,6 +79,11 @@ def webhook_get(hub_challenge: Annotated[int |None, Query(alias="hub.challenge")
 @app.post("/webhook")
 def webhook_post(data: WebhookBody):
     handle(data.sender(), data.text())
+
+
+@app.get("/")
+def status_get():
+    return HTMLResponse(f'<html><body>{status().replace("\n", "<br />\n")}</body></hmtl>')
 
 
 if __name__ == "__main__":
