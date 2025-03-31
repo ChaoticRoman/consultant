1fe8015f55fa3f9c96cb2cf7a5c850be60b47177
Roman Pavelka <roman.pavelka.asi@gmail.com>
2025-03-31 13:27:53 +0000

    Do not DoS Meta API

--- send.py
+++ send.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import requests
 
@@ -20,6 +21,7 @@ def send(number="420736452265", message="Hello, user!", debug=False):
     chunks = split_text_into_chunks(message, max_length=4096)
     for chunk in chunks:
         send_single(number, chunk, debug)
+        time.sleep(1)
 
 
 def send_single(number="420736452265", message="Hello, user!", debug=False):
